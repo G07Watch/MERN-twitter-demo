fbd887cb6155f6f76528e511e8063a0e4f2e78f8
Garvin <gming27@gmail.com>
2019-10-19 14:34:43 -0400

    creates server script for nodemon app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("App is running"));
 
 const port = process.env.PORT || 5000;
 
