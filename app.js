4a8a16cbffeb98ea9d4df30b7228838325d1887d
Garvin <gming27@gmail.com>
2019-10-19 16:38:18 -0400

    successfully establishes connection to MongoDb

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 // import mongoose from 'mongoose';
 // import express from 'express';
-// import {mongoURI} from './config/keys';
+// import db from './config/keys';
 // const db = mongoURI;
 
-const mongoose = require('mongoose');
-const express = require("express");
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI
 
+
 mongoose
-.connect(db, { useNewUrlParser: true })
-.then( () => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("App is running"));
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
