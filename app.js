24e0d237474d967d9430890e8935e07fc847cdd6
Garvin <gming27@gmail.com>
2019-10-19 15:14:22 -0400

    corrects and error in config keys string

--- app.js
+++ app.js
@@ -3,12 +3,10 @@
 // import {mongoURI} from './config/keys';
 // const db = mongoURI;
 
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-
-const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI
-
 
 mongoose
 .connect(db, { useNewUrlParser: true })
@@ -16,8 +14,6 @@ mongoose
 .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("App is running"));
-
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
