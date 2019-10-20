eb5d425bfd0942054cebb3c06b185b93a10da68b
Garvin <gming27@gmail.com>
2019-10-20 13:03:10 -0400

    creates login route for user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,10 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
 
+router.get("/test", (req, res) => {
+  res.json({ msg: "This is the user route" });
+})
+
 router.post('/register', (req, res)=>{
   User.findOne({ email: req.body.email })
   .then(user =>{
@@ -17,7 +21,7 @@ router.post('/register', (req, res)=>{
       })
 
       bcrypt.genSalt(10, (err, salt)=>{
-        bcrypt.hash(newUser.password, salt, (err,hash) =>{
+        bcrypt.hash(newUser.password, salt, (err, hash) =>{
           if (err) throw err;
           newUser.password = hash;
           newUser.save()
@@ -30,11 +34,26 @@ router.post('/register', (req, res)=>{
   })
 })
 
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: 'This user does not exist' });
+      }
 
-
-router.get("/test", (req, res) => {
-  res.json({ msg: "This is the user route" });
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        })
+    })
 })
+
 
 module.exports = router;
