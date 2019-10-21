25543955a28d32bdc1ea890ba0392027720bcd11
Garvin <gming27@gmail.com>
2019-10-21 01:33:22 -0400

    removes defunct nav.css and corrects main path

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,7 +4,7 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import TweetsContainer from './tweets/tweets_container';
-import MainPage from './main/main_page';
+import MainPage from './main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
