530282128c3e36e3bdd70f1d0d6318cfbe24a96e
Garvin <gming27@gmail.com>
2019-10-21 03:19:43 -0400

    moves if check for production

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// import express from 'express';
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
@@ -9,17 +8,18 @@ const bodyParser = require('body-parser');
 const passport = require('passport'); 
 const path = require('path');
 
+
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
-
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
 
 app.get("/", (req, res) =>{
   res.send("App is running")
